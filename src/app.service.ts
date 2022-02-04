26b2fde601e8e97f44ddbcf720978304c88f1aea
Kacper Salek <kacper.salek32@gmail.com>
2022-02-04 12:57:11 +0100

    corrected data type annotation and filter habitable planets

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CsvParser } from 'nest-csv-parser';
 import * as fs from 'fs';
 
-class Entity {
+class PlanetData {
   kepoi_name: string;
   koi_disposition: string;
   koi_insol: number;
@@ -10,24 +10,31 @@ class Entity {
 }
 
 type EntityFromStream = {
-  list: Entity[];
+  list: PlanetData[];
 };
 
 @Injectable()
 export class AppService {
   constructor(private readonly csvParser: CsvParser) {}
 
-  planetsData: Entity[] = [];
+  planetsData: PlanetData[] = [];
 
   async parse() {
     const stream = fs.createReadStream('kepler_data.csv');
 
+    function isHabitablePlanet(planetData) {
+      return planetData['koi_disposition'] === 'CONFIRMED';
+    }
+
     this.planetsData = await this.csvParser
-      .parse(stream, Entity, null, null, {
+      .parse(stream, PlanetData, null, null, {
         strict: true,
         separator: ',',
       })
-      .then((entities: EntityFromStream) => entities['list']);
+      .then((entities: EntityFromStream) => entities['list'])
+      .then((entities) => entities.filter(isHabitablePlanet));
+
+    console.log(this.planetsData.length);
 
     return this.planetsData;
   }
